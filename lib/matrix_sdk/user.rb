005222fc37a0c4539a4c98f345c0ec742ec6b7b0
Alexander Olofsson <ace@haxalot.com>
2018-05-10 16:58:57 +0200

    Fully implement the user class

--- lib/matrix_sdk/user.rb
+++ lib/matrix_sdk/user.rb
@@ -2,17 +2,44 @@ require 'matrix_sdk/extensions'
 
 module MatrixSdk
   class User
-    attr_reader :id, :client, :display_name
+    attr_reader :id, :client
 
     def initialize(client, id, data = {})
       @client = client
       @id = id
 
       @display_name = nil
+      @avatar_url = nil
 
       data.each do |k, v|
         instance_variable_set("@#{k}", v) if instance_variable_defined? "@#{k}"
       end
+    end
+
+    def display_name
+      @display_name ||= client.api.get_display_name(id)[:displayname]
+    end
+
+    def display_name=(name)
+      client.api.set_display_name(id, name)
+      @display_name = name
+    rescue MatrixError
+      nil
+    end
+
+    def friendly_name
+      display_name || id
+    end
+
+    def avatar_url
+      @avatar_url ||= client.api.get_avatar_url(id)[:avatar_url]
+    end
+
+    def avatar_url=(url)
+      client.api.set_avatar_url(id, url)
+      @avatar_url = url
+    rescue MatrixError
+      nil
     end
   end
 end
