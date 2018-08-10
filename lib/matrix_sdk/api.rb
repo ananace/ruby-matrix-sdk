71dbeaafc3967bf5c555067cdd9083d8159a6097
Alexander "Ace" Olofsson <ace@haxalot.com>
2018-08-10 10:03:39 +0200

    Make the version response nicer as string

--- lib/matrix_sdk/api.rb
+++ lib/matrix_sdk/api.rb
@@ -94,7 +94,13 @@ module MatrixSdk
     # Gets the server version
     # @note This uses the unstable federation/v1 API
     def server_version
-      Response.new self, request(:get, :federation_v1, '/version').server
+      Response.new(self, request(:get, :federation_v1, '/version').server).tap do |resp|
+        resp.instance_eval <<-'CODE', __FILE__, __LINE__ - 1
+          def to_s
+            "#{name} #{version}"
+          end
+        CODE
+      end
     end
 
     # Runs the client API /sync method
