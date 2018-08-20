c5a1c98661c90c1086f01870f459110bcc4892c5
Alexander "Ace" Olofsson <ace@haxalot.com>
2018-08-20 06:59:23 +0200

    Add token login method to client

--- lib/matrix_sdk/client.rb
+++ lib/matrix_sdk/client.rb
@@ -102,6 +102,19 @@ module MatrixSdk
       sync(timeout: params.fetch(:sync_timeout, 15), full_state: params.fetch(:full_state, false)) unless params[:no_sync]
     end
 
+    def login_with_token(username, token, params = {})
+      username = username.to_s unless username.is_a?(String)
+      token = token.to_s unless token.is_a?(String)
+
+      raise ArgumentError, "Username can't be nil or empty" if username.nil? || username.empty?
+      raise ArgumentError, "Token can't be nil or empty" if token.nil? || token.empty?
+
+      data = api.login(user: username, token: token, type: 'm.login.token')
+      post_authentication(data)
+
+      sync(timeout: params.fetch(:sync_timeout, 15), full_state: params.fetch(:full_state, false)) unless params[:no_sync]
+    end
+
     def logout
       api.logout
       @api.access_token = nil
