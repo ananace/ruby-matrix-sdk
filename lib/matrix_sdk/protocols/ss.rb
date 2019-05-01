3ac63ebc0adbdc4efab78f75dbc4e641acd94fc8
Alexander Olofsson <ace@haxalot.com>
2019-05-01 23:31:46 +0200

    Api: Fix S2S indentation

--- lib/matrix_sdk/protocols/ss.rb
+++ lib/matrix_sdk/protocols/ss.rb
@@ -1,12 +1,12 @@
 module MatrixSdk::Protocols::SS
-    # Gets the server version
-    def server_version
-      Response.new(self, request(:get, :federation_v1, '/version').server).tap do |resp|
-        resp.instance_eval <<-'CODE', __FILE__, __LINE__ + 1
-          def to_s
-            "#{name} #{version}"
-          end
-        CODE
-      end
+  # Gets the server version
+  def server_version
+    Response.new(self, request(:get, :federation_v1, '/version').server).tap do |resp|
+      resp.instance_eval <<-'CODE', __FILE__, __LINE__ + 1
+        def to_s
+          "#{name} #{version}"
+        end
+      CODE
     end
+  end
 end
