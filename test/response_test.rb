6a1cfe49322d00b7e0ef6606d47fd95e33e49e74
Alexander "Ace" Olofsson <ace@haxalot.com>
2019-10-28 07:41:41 +0100

    Add quick test for response creation

---
+++ test/response_test.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class ResponseTest < Test::Unit::TestCase
+  def setup
+    @http = mock
+    @http.stubs(:active?).returns(true)
+
+    @api = MatrixSdk::Api.new 'https://example.com'
+    @api.instance_variable_set :@http, @http
+    @api.stubs(:print_http)
+  end
+
+  def test_creation
+    data = { test_key: 'value' }
+    response = MatrixSdk::Response.new(@api, data)
+
+    assert_equal @api, response.api
+    assert_equal 'value', response.test_key
+  end
+
+  def test_creation_failure
+    data = [{ key: 'value' }]
+    assert_raises(ArgumentError) { MatrixSdk::Response.new(@api, data) }
+  end
+end
