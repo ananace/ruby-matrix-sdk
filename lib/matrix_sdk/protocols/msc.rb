dc72707a53fafe3413a6f48ab3c80fdc8b575eef
Alexander "Ace" Olofsson <ace@haxalot.com>
2020-03-16 08:06:24 +0100

    Include a unique stream id for SSE debug

--- lib/matrix_sdk/protocols/msc.rb
+++ lib/matrix_sdk/protocols/msc.rb
@@ -93,7 +93,9 @@ module MatrixSdk::Protocols::MSC
           end
         end
 
-        logger.debug 'MSC2108: Starting SSE stream.'
+        stream_id = ('A'..'Z').to_a.sample(4).join
+
+        logger.debug "MSC2108 : #{stream_id} : Starting SSE stream."
 
         buffer = ''
         response.read_body do |chunk|
@@ -117,8 +119,8 @@ module MatrixSdk::Protocols::MSC
               /^id:(.+)$/.match(part) do |m_id|
                 id = m_id[1].strip
               end
-              /:(.+)$/.match(part) do |m_comment|
-                logger.debug "MSC2108: Received comment '#{m_comment[1].strip}'"
+              /^:(.+)$/.match(part) do |m_comment|
+                logger.debug "MSC2108 : #{stream_id} : Received comment '#{m_comment[1].strip}'"
               end
             end
 
