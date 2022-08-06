075c31a5dfb530aa51b0dfed744e60178d483303
Alexander Olofsson <ace@haxalot.com>
2022-08-07 01:56:41 +0200

    util/extensions: Slight improvements

--- lib/matrix_sdk/util/extensions.rb
+++ lib/matrix_sdk/util/extensions.rb
@@ -62,7 +62,7 @@ module MatrixSdk
         end
 
         def pretty_print(pp)
-          pp.pp_object(self)
+          pp.pp(self)
         end
 
         alias inspect pretty_print_inspect
@@ -74,7 +74,7 @@ module MatrixSdk
     def logger
       return MatrixSdk.logger if MatrixSdk.global_logger?
 
-      @logger ||= ::Logging.logger[self]
+      @logger || ::Logging.logger[self]
     end
 
     def logger=(logger)
