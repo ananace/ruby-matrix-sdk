2e61bd36a8220846045dee5d4ab15c6d171c918f
Alexander "Ace" Olofsson <ace@haxalot.com>
2022-08-11 23:37:30 +0200

    Improve another Api.new_for_domain test

--- test/api_creation_test.rb
+++ test/api_creation_test.rb
@@ -67,11 +67,19 @@ class ApiTest < Test::Unit::TestCase
       .expects(:getresource)
       .never
 
-    ::Net::HTTP
-      .any_instance
+    http_mock = mock
+
+    http_mock
       .expects(:get)
       .with('/.well-known/matrix/client')
       .returns(OpenStruct.new(body: '{"m.homeserver":{"base_url":"https://matrix.example.com"}}'))
+
+    Net::HTTP
+      .expects(:start)
+      .with('example.com', 443, use_ssl: true, open_timeout: 5, read_timeout: 5, write_timeout: 5)
+      .with_block_given
+      .yields(http_mock)
+      .returns('{"m.homeserver":{"base_url":"https://matrix.example.com"}}')
 
     MatrixSdk::Api
       .expects(:new)
